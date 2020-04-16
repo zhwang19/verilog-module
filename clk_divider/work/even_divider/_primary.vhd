0bf22c1f9145e9d7452f4471cafab7fca2ed3510
wzh <zhwang19@mails.jlu.edu.cn>
2020-04-16 13:33:35 +0800

    delete clk_divider

--- clk_divider/work/even_divider/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity even_divider is
-    port(
-        clk_in          : in     vl_logic;
-        rst_n           : in     vl_logic;
-        clk_out_even    : out    vl_logic
-    );
-end even_divider;
--- clk_divider/work/tb_divider/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_divider is
-end tb_divider;
